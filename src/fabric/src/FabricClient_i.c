373636d89137d8b7427e4ea8da07e10db3fea842
Youyuan Wu <youyuanwu@outlook.com>
2023-08-03 22:46:12 -0700

    add fabric transport to winmd

--- src/fabric/src/FabricClient_i.c
+++ src/fabric/src/FabricClient_i.c
@@ -8,14 +8,6 @@
  /* File created by MIDL compiler version 8.01.0628 */
 /* at Mon Jan 18 19:14:07 2038
  */
-/* Compiler settings for D:/code/cpp/fabric-metadata/idl/FabricClient.idl:
-    Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 8.01.0628 
-    protocol : dce , ms_ext, c_ext, robust
-    error checks: allocation ref bounds_check enum stub_data 
-    VC __declspec() decoration level: 
-         __declspec(uuid()), __declspec(selectany), __declspec(novtable)
-         DECLSPEC_UUID(), MIDL_INTERFACE()
-*/
 /* @@MIDL_FILE_HEADING(  ) */
 
 #pragma warning( disable: 4049 )  /* more than 64k source lines */
